76a29ac22a3bfb753e3efcfb19a96c8fa9dabdc9
jjbrowndesign <43221096+jjbrowndesign@users.noreply.github.com>
2023-11-06 19:03:35 +0000

    Adding the finance website, updating contact us to a button

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
-
 function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
   } else {
-    x.className = 'navtoggle';
+    x.className = "navtoggle";
   }
 }
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-150px";
+  }
+  prevScrollpos = currentScrollPos;
+};
