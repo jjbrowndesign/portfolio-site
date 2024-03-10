9615160a8a626b257d07abf845b752ea8272c6f3
jjbrowndesign <43221096+jjbrowndesign@users.noreply.github.com>
2024-03-10 11:05:38 +0000

    Stopping navbar from hiding when user goes back to top of page

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,8 @@ window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
+  } else if (prevScrollpos < 25) {
+    document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-150px";
   }
